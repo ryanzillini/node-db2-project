8a7fe4187c61a1e4bc94d682fc1b659a6befe0df
Ryan Zillini <nyrtrading134@gmail.com>
2023-09-06 13:42:30 -0400

    middleware WIP

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,35 @@
+const Car = require("./cars-model");
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
-}
+  const carId = req.params.id;
+  if (!carId.trim()) {
+    res.status(404).json({ message: `car with id ${carId} i is not found` });
+  } else {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body
+  if(!vin || !make || !model || !mileage) {
+    res.status(400).json({message: `${} is missing`})
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+  
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,7 @@
 // DO YOUR MAGIC
+const express = require("express");
+const router = express.Router();
+
+const Car = require("./cars-model");
+
+module.exports = router;
