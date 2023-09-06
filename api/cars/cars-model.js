a0856211b6f3db4a0a4d48f4a21687bb3bcdefa7
Ryan Zillini <nyrtrading134@gmail.com>
2023-09-06 13:42:58 -0400

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,27 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db("cars");
+};
 
-const getById = () => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db("cars").where({ id }).first();
+};
 
-const create = () => {
+const create = (account) => {
   // DO YOUR MAGIC
-}
+  return db("cars")
+    .insert(account)
+    .then((id) => {
+      console.log(account);
+      return getById(id);
+    });
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111",
+    make: "ford",
+    model: "F150",
+    mileage: 122000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "2222222222",
+    make: "toyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "33333333333",
+    make: "acura",
+    model: "TLX",
+    mileage: 20000,
+    title: "salvage",
+    transmission: "automatic",
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
