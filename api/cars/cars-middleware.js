ad246506565c189d768859e00a84e9057a43630a
Ryan Zillini <nyrtrading134@gmail.com>
2023-09-08 11:05:40 -0400

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -12,15 +12,21 @@ const checkCarId = (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-  const { vin, make, model, mileage } = req.body
-  if(!vin || !make || !model || !mileage) {
-    res.status(400).json({message: `${} is missing`})
-  }
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    res.status(400).json({ message: `vin number is missing` });
+  } else if (!make) {
+    res.status(400).json({ message: `make is missing` });
+  } else if (!model) {
+    res.status(400).json({ message: `model is missing` });
+  } else if (!mileage) {
+    res.status(400).json({ message: `mileage is missing` });
+  } else next();
 };
 
-const checkVinNumberValid = (req, res, next) => {
+const checkVinNumberValid = async (req, res, next) => {
   // DO YOUR MAGIC
-  
+  const vinNum = req.body;
 };
 
 const checkVinNumberUnique = (req, res, next) => {
--- api/server.js
+++ api/server.js
@@ -4,4 +4,14 @@ const server = express();
 
 // DO YOUR MAGIC
 
+server.use(express.json());
+
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found" });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
 module.exports = server;
